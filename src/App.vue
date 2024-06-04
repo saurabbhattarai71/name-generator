c44be25f4573670e6774596c0a1576540f8af61b
saurabbhattarai71 <saurabbhattarai@thefireflytech.com>
2024-06-04 17:14:13 +0545

    second commmit for the day

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
-<script setup>
+<script>
+import { RouterView } from 'vue-router' 
+import Navbar from '../src/components/Navbar.vue'
+import Herosection from './components/Herosection.vue'; 
+import Ourservices from './components/Ourservices.vue';
+import Footer from './components/Footer.vue'; 
 
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Herosection,
+    Ourservices,
+    Footer,
+  }
+}
 </script>
 
-<template> 
-  <h1 class = "text-3xl font-bold underline">Hello</h1>
+<template>   
+  <Navbar />
+  <Herosection /> 
+  <Ourservices /> 
+  <Footer />
+  <RouterView />
 </template>
 
 <style scoped>
